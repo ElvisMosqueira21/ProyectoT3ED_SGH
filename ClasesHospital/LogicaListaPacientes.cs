b41a4c53be05559ff73feeaf6f9b4d700c4c9e3a
Elvis  Mosqueira Home <N00447800@upn.pe>
2026-05-20 16:56:01 -0500

    Agregar archivos de proyecto.

---
+++ ClasesHospital/LogicaListaPacientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHospital
+{
+    public class LogicaListaPacientes
+    {
+        // Primer nodo de la lista
+        private NodoPaciente cabeza;
+
+        // Constructor
+        public LogicaListaPacientes()
+        {
+            cabeza = null;
+        }
+        //Metodos
+    }
+}
---
+++ ClasesHospital/Paciente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHospital
+{
+    public class Paciente
+    {
+        //Atributos
+        public int Codigo {  get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string DNI { get; set; }
+        public string Telefono { get; set; }
+        //Constructor vacio
+        public Paciente() 
+        {
+
+        }
+        //Constructor con parametros
+        public Paciente(int codigo, string nombre, int edad, string dni, string telefono) 
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Edad = edad;
+            DNI = dni;
+            Telefono = telefono;
+        }
+        // Método para mostrar información
+        public override string ToString()
+        {
+            return $"{Codigo} - {Nombre} - {DNI}";
+        }
+
+    }
+}
